cc75e664bd6e4b521393dd44bd05672758b4915b
JUAN LITVIN <42647413@ort>
2017-05-29 10:37:37 -0300

    Add devices's list btnRemove listener
    Signed-off-by: JUAN LITVIN <42647413@ort>

--- Phone/app/src/main/java/com/juanlitvin/fenix/DeviceListAdapter.java
+++ Phone/app/src/main/java/com/juanlitvin/fenix/DeviceListAdapter.java
@@ -1,17 +1,25 @@
 package com.juanlitvin.fenix;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.support.annotation.LayoutRes;
 import android.support.annotation.NonNull;
+import android.support.v4.util.ArrayMap;
+import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.loopj.android.http.RequestParams;
 
 import org.w3c.dom.Text;
 
 import java.util.List;
+import java.util.Map;
 
 public class DeviceListAdapter extends ArrayAdapter {
 
@@ -28,7 +36,7 @@ public class DeviceListAdapter extends ArrayAdapter {
     }
 
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
+    public View getView(final int position, View convertView, ViewGroup parent) {
 
         View v = convertView;
 
@@ -36,15 +44,99 @@ public class DeviceListAdapter extends ArrayAdapter {
         }
         v = LayoutInflater.from(getContext()).inflate(R.layout.devices_list, null);
 
-        Device device = devices.get(position);
+        final Device device = devices.get(position);
 
         TextView lblDeviceName = (TextView) v.findViewById(R.id.lblDeviceName);
         TextView lblUserName = (TextView) v.findViewById(R.id.lblUserName);
+        ImageButton btnLogout = (ImageButton) v.findViewById(R.id.btnLogout);
+        ImageButton btnEdit = (ImageButton) v.findViewById(R.id.btnEdit);
+        ImageButton btnRemove = (ImageButton) v.findViewById(R.id.btnRemove);
+
+        //listeners
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        btnEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        btnRemove.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showYesNoDialog("Remove?", "Would you like to remove the device \"" + device.getName() + "\". This device will no longer show as owned as you, unless you register it again.", new YesNoHandler() {
+                    @Override
+                    public void onYes() {
+                        final Device device = User.getDevices().get(position);
+                        User.getDevices().remove(device);
+                        notifyDataSetChanged();
+
+                        RequestParams params = new RequestParams();
+                        params.put("device-id", device.getIdDevice());
+
+                        Map<String, String> headers = new ArrayMap<>();
+                        headers.put("Token", "?QKGe,q$uxkwi7cJ-h4zsuW],^{BFEurhNkfW~-TAnUGc%TGJ4PqmIIp3(FNBj%O");
+                        headers.put("Auth", User.getApiKey());
+
+                        RESTClient.post("http://juanlitvin.com/api/aguila/v1/index.php/user/mirror/remove", params, headers, new RESTClient.ResponseHandler() {
+                            @Override
+                            public void onSuccess(int code, String responseBody) {
+
+                            }
+
+                            @Override
+                            public void onFailure(int code, String responseBody, Throwable error) {
+                                Toast.makeText(getContext(), "Hubo un error al intentar eliminar el dispositivo", Toast.LENGTH_SHORT).show();
+                                User.getDevices().add(position, device);
+                                notifyDataSetChanged();
+                            }
+                        });
+                    }
+
+                    @Override
+                    public void onNo() {
+
+                    }
+                });
+            }
+        });
 
         lblDeviceName.setText(device.getName());
         lblUserName.setText(device.getOwnerName());
 
         return v;
+    }
+
+    private void showYesNoDialog(String title, String message, final YesNoHandler handler) {
+        new AlertDialog.Builder(getContext())
+                .setTitle(title.length() == 0 ? null : title)
+                .setMessage(message)
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        handler.onYes();
+                        dialog.dismiss();
+                    }
+                })
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        handler.onNo();
+                        dialog.dismiss();
+                    }
+                })
+                .create().show();
+    }
+
+    private interface YesNoHandler {
+        void onYes();
+        void onNo();
     }
 
 }
